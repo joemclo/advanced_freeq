cb0d53a64d3e49e0b4e6e14786e41f4f8d2dedfe
joemclo8 <joemclo8@gmail.com>
2022-06-10 19:06:56 +0100

    Simple Advance freeq to create report of word occurances in pdf files

--- advanced_freeq.py
+++ advanced_freeq.py
@@ -1,100 +1,86 @@
 #!/usr/bin/env python3
 
-"""advanced_freeq
-
-Usage:
-    ./advanced_freeq -t <txtname>  [-o <output>] [-s <mastered>]
-    ./advanced_freeq -p <pdfname>  [-o <output>] [-s <mastered>]
-    ./advanced_freeq -m <mobiname> [-o <output>] [-s <mastered>]
-    ./advanced_freeq -e <epubname> [-o <output>] [-s <mastered>]
-
-Examples:
-    ./advanced_freeq -i txtname.txt -o bookfreeq.csv
-    ./advanced_freeq -p txtname.pdf -o bookfreeq.csv
-    ./advanced_freeq -p txtname.pdf -o bookfreeq.csv -s mastered.csv
-
-Options:
-    -h --help           Show this screen.
-    -v --version        Show version
-    -t --txt            Input Text file
-    -p --pdf            Input PDF file
-    -m --mobi           Input mobi file
-    -e --epub           Input Epub file
-    -o --output         Output frequency file
-    -s --mastered       Mastered vocabularies file
-"""
-
-from docopt import docopt
-
-if __name__ == '__main__':
-    arguments = docopt(__doc__, version='advanced freeq 0.2')
 
 import os
 import numpy as np
 import pandas as pd
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from io import StringIO
 
-if arguments['--txt'] == True:
-    os.system(
-        './freeq.py -i %s -o .book_freeq.csv' %
-        arguments['<txtname>']
-    )
-elif arguments['--pdf'] == True:
-    from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-    from pdfminer.converter import TextConverter
-    from pdfminer.layout import LAParams
-    from pdfminer.pdfpage import PDFPage
-    from io import StringIO
+path_to_open = "./"
 
-    def convert_pdf_to_txt(path):
-        rsrcmgr = PDFResourceManager()
-        retstr = StringIO()
-        codec = 'utf-8'
-        laparams = LAParams()
-        device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
-        fp = open(path, 'rb')
-        interpreter = PDFPageInterpreter(rsrcmgr, device)
-        password = ""
-        maxpages = 0
-        caching = True
-        pagenos=set()
-        for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
-            interpreter.process_page(page)
-        fp.close()
-        device.close()
-        str = retstr.getvalue()
-        retstr.close()
-        with open('.book.txt', 'w') as book:
-            book.write('%s' % str)
-    convert_pdf_to_txt(arguments['<pdfname>'])
-    os.system('./freeq.py -i .book.txt -o .book_freeq.csv')
+files = os.listdir(os.path.expanduser(path_to_open))
+print(files)
 
-elif arguments['--mobi'] == True:
-    os.system('ebook-convert %s .book.txt' %arguments['<mobiname>'])
-    os.system('./freeq.py -i .book.txt -o .book_freeq.csv')
+word_list = pd.read_csv("./word_list.csv", names=["Word"])
 
-else:
-    os.system('ebook-convert %s .book.txt' %arguments['<epubname>'])
-    os.system('./freeq.py -i .book.txt -o .book_freeq.csv')
+df_master = word_list
 
-os.system("sed -i 's/^ *//g' .book_freeq.csv")
-os.system("sed -i 's/ /,/g' .book_freeq.csv")
+for fileName in files:
 
-df_book = pd.read_csv('.book_freeq.csv', names=['Freq', 'Word'])
-f = lambda x: len(str(x)) > 2
-df_book = df_book[df_book['Word'].apply(f)]
+    if not fileName.endswith(".pdf"):
+        continue
 
-if arguments['--mastered'] == False:
-    df_coca = pd.read_csv(
-        'COCA_top5000.csv'
-    ).loc[:,['Rank','Word']]
-    df_freq = df_book[~df_book['Word'].isin(df_coca['Word'].iloc[:1000])]
-    df_freq.to_csv('%s' % arguments['<output>'], index = None)
-    print('All your freeq words are in %s' % arguments['<output>'])
+    filePath = os.path.expanduser(path_to_open) + fileName
 
-else:
-    df_mastered = pd.read_csv(
-        '%s' % arguments['<mastered>'], names = ['Index', 'Word']
-    )
-    df_freq = df_book[~df_book['Word'].isin(df_mastered['Word'])]
-    df_freq.to_csv('%s' % arguments['<output>'], index = None)
-    print('All your word frequency are in %s' % arguments['<output>'])
+    output_filename = "%s_words.csv" % fileName
+
+    if not os.path.exists(output_filename):
+
+        def convert_pdf_to_txt(path):
+            rsrcmgr = PDFResourceManager()
+            retstr = StringIO()
+            codec = "utf-8"
+            laparams = LAParams()
+            device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+            fp = open(path, "rb")
+            interpreter = PDFPageInterpreter(rsrcmgr, device)
+            password = ""
+            maxpages = 0
+            caching = True
+            pagenos = set()
+            for page in PDFPage.get_pages(
+                fp,
+                pagenos,
+                maxpages=maxpages,
+                password=password,
+                caching=caching,
+                check_extractable=False,
+            ):
+                interpreter.process_page(page)
+            fp.close()
+            device.close()
+            str = retstr.getvalue()
+            retstr.close()
+            with open(".book.txt", "w") as book:
+                book.write("%s" % str)
+
+        convert_pdf_to_txt(filePath)
+        os.system("./freeq.py -i .book.txt -o .book_freeq.csv")
+
+        os.system("sed -i 's/^ *//g' .book_freeq.csv")
+        os.system("sed -i 's/ /,/g' .book_freeq.csv")
+
+        df_book = pd.read_csv(".book_freeq.csv", names=[fileName, "Word"])
+        f = lambda x: len(str(x)) > 2
+        df_book = df_book[df_book["Word"].apply(f)]
+
+        print(word_list["Word"])
+
+        df_freq = df_book[df_book["Word"].isin(word_list["Word"])]
+
+        df_freq = df_freq.sort_values("Word")
+
+        word = df_freq["Word"]
+        df_freq.drop(labels=["Word"], axis=1, inplace=True)
+        df_freq.insert(0, "Word", word)
+
+        df_master = pd.merge(df_master, df_freq, how="outer", on="Word")
+
+        print(df_master)
+
+df_master = df_master.replace(np.NaN, 0)
+df_master.to_csv("word_occurrences.csv", index=None)
